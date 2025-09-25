c05159023a62d63528a3877889fd48f6e9b3d3f3
PauaPrime <89046893+PauaPrime@users.noreply.github.com>
2025-09-25 13:29:39 +1200

    Delete App2.js

--- App2.js
+++
@@ -1,7 +0,0 @@
-// ... (previous code)
-return (
-  // ...
-  {address && <Dashboard address={address} isPaid={isPaid} trialActive={trialActive} />}
-  {!trialActive && !isPaid && <UpgradeModal onSuccess={handlePaymentSuccess} trialActive={trialActive} />}
-  // ...
-);
